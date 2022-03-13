4c57e4d9595680406b40a4a835973eb798458a38
poormonitor <poormonitor@outlook.com>
2022-03-13 10:24:29 +0800

    update gunicorn

--- run.py
+++ run.py
@@ -1,15 +1,35 @@
-from gunicorn.app.base import BaseApplication
- 
-class Application(BaseApplication):
+import multiprocessing
+import gunicorn.app.base
+from server import app
+
+
+def number_of_workers():
+    return (multiprocessing.cpu_count() * 2) + 1
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
     def load_config(self):
-        s = self.cfg.set
-        s('bind', "0.0.0.0:5000")
-        s('workers', 4)
-        s('timeout', 60)
- 
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
     def load(self):
-        from server import app
-        return app
- 
+        return self.application
+
+
 if __name__ == '__main__':
-    Application().run()
+    options = {
+        'bind': '%s:%s' % ('127.0.0.1', '5000'),
+        'workers': number_of_workers(),
+    }
+    StandaloneApplication(app, options).run()
