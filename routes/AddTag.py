a99f7cd54b91a77366f6057004efcff8a54f52a3
poormonitor <poormonitor@outlook.com>
2022-06-19 18:38:07 +0800

    fix width

--- routes/AddTag.py
+++ routes/AddTag.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, session, request, url_for
-from func import valid_csrf, admin_required
+from func import get_school_name, unifyBracket, valid_csrf, admin_required
 from models import db
 from models.Tag import Tag
 from models.Univ import Univ
@@ -27,6 +27,8 @@ def addtag():
         db.session.flush()
     tag_id = a.tid
     for i in schools.splitlines():
+        i = get_school_name(i)
+        i = unifyBracket(i)
         if (a := Univ.query.filter_by(uname=i).first()) is None:
             a = Univ(uname=i)
             db.session.add(a)
