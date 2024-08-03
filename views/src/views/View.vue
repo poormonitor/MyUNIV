064fb0b8bc1a86c95f27b87ac4a2e34fe0c698ab
poormonitor <poormonitor@outlook.com>
2024-08-03 09:51:30 +0800

    fix: view page jumping

--- views/src/views/View.vue
+++ views/src/views/View.vue
@@ -2,15 +2,17 @@
 
 <template>
     <div class="mx-8 w-auto md:mx-auto md:w-[70vw] lg:w-[60vw] my-8 md:my-10">
-        <Suspense>
-            <router-view />
-            <template #fallback>
-                <div class="flex place-content-center mt-[40vh]">
-                    <n-spin>
-                        <template #description> 加载中 </template>
-                    </n-spin>
-                </div>
-            </template>
-        </Suspense>
+        <router-view v-slot="{ Component }">
+            <Suspense :key="Component">
+                <component :is="Component" />
+                <template #fallback>
+                    <div class="flex place-content-center mt-[40vh]">
+                        <n-spin>
+                            <template #description> 加载中 </template>
+                        </n-spin>
+                    </div>
+                </template>
+            </Suspense>
+        </router-view>
     </div>
 </template>
